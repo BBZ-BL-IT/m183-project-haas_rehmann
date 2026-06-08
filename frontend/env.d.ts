12f27224d99f5a67fbe7ddea24292331f0076690
Fluri07 <nico.rehmann@bbzbl-it.ch>
2026-06-08 13:52:27 +0200

    chore(env): type vite env vars

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_OAUTH_LOGIN_PATH: string
+  readonly VITE_OAUTH_LOGOUT_PATH: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
